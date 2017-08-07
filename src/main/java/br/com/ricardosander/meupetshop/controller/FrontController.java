923ab086575f516ab2baab4d227618d21d75bafd
Ricardo Lopes <ricardo.lopes@commbox.com.br>
2017-08-07 13:22:30 -0300

    Iniciando criação no Request handler.

--- src/main/java/br/com/ricardosander/meupetshop/controller/FrontController.java
+++ src/main/java/br/com/ricardosander/meupetshop/controller/FrontController.java
@@ -13,8 +13,26 @@ public class FrontController extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        String tarefa = "br.com.ricardosander.meupetshop.http." + req.getMethod().toLowerCase() + "";
+
+        this.getAction(req);
+
         String destino = "WEB-INF/pages/404.jsp";
         req.getRequestDispatcher(destino).forward(req, resp);
+    }
+
+    private void getAction(HttpServletRequest request) {
+
+        String[] resources = request.getRequestURI().split("/");
+
+        if (resources.length < 2) {
+            return;//TODO retornar index.
+        }
+
+        String method = request.getMethod().toLowerCase();
+        String pacote = "br.com.ricardosander.meupetshop." + method + "." + resources[1].substring(0, 1).toUpperCase() + resources[1].substring(1);
+
+        System.out.println(pacote);
     }
 
 }
