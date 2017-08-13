23b84a4d9a89c3b106bc844d0537752a03065b17
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-08-13 04:54:14 -0300

    Criando servlet para Login.

---
+++ src/main/java/br/com/ricardosander/meupetshop/servlets/Login.java
@@ -0,0 +1,19 @@
+package br.com.ricardosander.meupetshop.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Página de login.
+ */
+public class Login extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/pages/login.jsp").forward(req, resp);
+    }
+
+}
