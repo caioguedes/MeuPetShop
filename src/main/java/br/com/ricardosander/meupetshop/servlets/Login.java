ba3c8e9e7c0f2ae6c049d246cbad27c7ac3bd9d2
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-08-13 05:28:51 -0300

    Iniciando implementação do login.

--- src/main/java/br/com/ricardosander/meupetshop/servlets/Login.java
+++ src/main/java/br/com/ricardosander/meupetshop/servlets/Login.java
@@ -1,5 +1,9 @@
 package br.com.ricardosander.meupetshop.servlets;
 
+import br.com.ricardosander.meupetshop.dao.UserDAO;
+import br.com.ricardosander.meupetshop.dao.UserDAOProvider;
+import br.com.ricardosander.meupetshop.model.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,4 +20,19 @@ public class Login extends HttpServlet {
         req.getRequestDispatcher("/WEB-INF/pages/login.jsp").forward(req, resp);
     }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String usuario = req.getParameter("usuario");
+        String senha = req.getParameter("senha");
+
+        UserDAO userDAO = new UserDAOProvider().newUserDAO();
+        User user = userDAO.find(usuario, senha);
+
+        if (user != null) {
+            req.getSession().setAttribute("loggedUser", user);
+        }
+
+        resp.sendRedirect("/");
+    }
 }
