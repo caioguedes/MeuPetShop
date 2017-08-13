00c0dabd8789b2645d8f123cb2effd2ec59b8cbd
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-08-13 05:25:56 -0300

    Classe provides para UserDAO.

---
+++ src/main/java/br/com/ricardosander/meupetshop/dao/UserDAOProvider.java
@@ -0,0 +1,15 @@
+package br.com.ricardosander.meupetshop.dao;
+
+/**
+ * Provider para UserDAO.
+ */
+public class UserDAOProvider {
+
+    /**
+     * @return Retorna uma instância da classe padrão.
+     */
+    public UserDAO newUserDAO() {
+        return new StaticUserDAO();
+    }
+
+}
