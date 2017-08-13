1672ba13b612e2e9acd0fbba1b79546fe2073ba3
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-08-13 02:16:26 -0300

    Criando handler para a index.

---
+++ src/main/java/br/com/ricardosander/meupetshop/http/get/Index.java
@@ -0,0 +1,18 @@
+package br.com.ricardosander.meupetshop.http.get;
+
+import br.com.ricardosander.meupetshop.http.HttpHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Página inicial.
+ */
+public class Index implements HttpHandler {
+
+    @Override
+    public String handler(HttpServletRequest req, HttpServletResponse resp) {
+        return "WEB-INF/index.jsp";;
+    }
+
+}
