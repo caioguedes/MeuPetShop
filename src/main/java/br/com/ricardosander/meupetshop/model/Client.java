0c13b51e40938c4e1515645c0ac73ba122c98319
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-08-17 01:14:45 -0300

    Iniciando criação do model de cliente.

---
+++ src/main/java/br/com/ricardosander/meupetshop/model/Client.java
@@ -0,0 +1,22 @@
+package br.com.ricardosander.meupetshop.model;
+
+public class Client {
+
+    private long id;
+
+    private String name;
+
+    public Client(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
