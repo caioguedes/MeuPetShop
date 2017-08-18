b02f3ed34ec9ab8e7934600e2b3dd5bd97b8ddd8
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-08-17 23:31:00 -0300

    Criando equals e hashcode para a classe User.

--- src/main/java/br/com/ricardosander/meupetshop/model/User.java
+++ src/main/java/br/com/ricardosander/meupetshop/model/User.java
@@ -17,4 +17,21 @@ public class User {
     public String getSenha() {
         return senha;
     }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
+    }
+
 }
