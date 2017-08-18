3bd153d5fcf39623ebe1e188aaca2006325bac94
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-08-17 23:33:41 -0300

    Criando provider para PetDAO.

---
+++ src/main/java/br/com/ricardosander/meupetshop/dao/PetDAOProvider.java
@@ -0,0 +1,15 @@
+package br.com.ricardosander.meupetshop.dao;
+
+/**
+ * Provider para PetDAO.
+ */
+public class PetDAOProvider {
+
+    /**
+     * @return Retorna uma instância da classe padrão.
+     */
+    public PetDAO newPetDAO() {
+        return new StaticPetDAO();
+    }
+
+}
