ba07e8e6c011a4a19f079d8c57757b7d4777e5f7
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-08-20 00:10:50 -0300

    Criando novo comportamento da interface e documentando.

--- src/main/java/br/com/ricardosander/meupetshop/dao/PetDAO.java
+++ src/main/java/br/com/ricardosander/meupetshop/dao/PetDAO.java
@@ -7,6 +7,18 @@ import java.util.List;
 
 public interface PetDAO {
 
+    /**
+     * Busca os pets do usuário.
+     * @param user Usuário do cadastro do pet.
+     * @return Lista de pets do usuário.
+     */
     List<Pet> find(User user);
+
+    /**
+     * Insere um novo pet.
+     * @param pet Novo Pet para cadastro.
+     * @return Se houve sucesso no cadastro.
+     */
+    boolean insert(Pet pet);
 
 }
