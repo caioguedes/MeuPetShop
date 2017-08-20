78222d1ee827e10e70a42699e3f529e8468ef662
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-08-19 23:26:57 -0300

    Adicionando novo comportamento a interface.

--- src/main/java/br/com/ricardosander/meupetshop/validator/Validator.java
+++ src/main/java/br/com/ricardosander/meupetshop/validator/Validator.java
@@ -1,10 +1,12 @@
 package br.com.ricardosander.meupetshop.validator;
 
 import java.io.InvalidObjectException;
+import java.util.List;
 
 /**
  * Interface que representa o comportamento de um validador.
  */
 public interface Validator {
     void validate() throws InvalidObjectException;
+    List<String> getErrors();
 }
