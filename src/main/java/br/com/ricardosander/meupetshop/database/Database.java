6eca787e8061e835b3c332c2880aa11f03870d34
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-09-23 14:55:58 -0300

    Mudando de criação de conexão para uso de pool de conexões.

--- src/main/java/br/com/ricardosander/meupetshop/database/Database.java
+++ src/main/java/br/com/ricardosander/meupetshop/database/Database.java
@@ -1,7 +1,10 @@
 package br.com.ricardosander.meupetshop.database;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
@@ -10,15 +13,36 @@ import java.sql.SQLException;
 public class Database {
 
     /**
+     * Pool de conexões.
+     */
+    private static DataSource pool;
+
+    public Database() throws SQLException, PropertyVetoException {
+
+        if (Database.pool == null) {
+
+            ComboPooledDataSource pool = new ComboPooledDataSource();
+            pool.setDriverClass("com.mysql.jdbc.Driver");
+            pool.setJdbcUrl("jdbc:mysql://127.0.0.1:3306/meupetshop");
+            pool.setUser("root");
+            pool.setPassword("123");
+            pool.setMinPoolSize(1);
+            pool.setAcquireIncrement(5);
+            pool.setMaxPoolSize(20);
+
+            Database.pool = pool;
+        }
+
+    }
+
+    /**
      * Cria e retorna uma nova conexão com o banco de dados.
+     *
      * @return Conexão com o banco de dados.
      * @throws SQLException Erro ao realizar conexão com o banco de dados.
      */
-    public static Connection getConnection() throws SQLException {
-
-        DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-
-        return DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/meupetshop", "root", "123");
+    public Connection getConnection() throws SQLException {
+        return Database.pool.getConnection();
     }
 
 }
