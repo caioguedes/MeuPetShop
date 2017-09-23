c6ff4ecf1afc8b3bf88ec73cc326e00f174f9090
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-09-23 15:43:38 -0300

    Mudando assinaturas dos métodos. Tornando classe generics. Adicionando documentação aos métodos.

--- src/main/java/br/com/ricardosander/meupetshop/validator/Validator.java
+++ src/main/java/br/com/ricardosander/meupetshop/validator/Validator.java
@@ -1,12 +1,23 @@
 package br.com.ricardosander.meupetshop.validator;
 
 import java.io.InvalidObjectException;
-import java.util.List;
+import java.util.Map;
 
 /**
  * Interface que representa o comportamento de um validador.
  */
-public interface Validator {
-    void validate() throws InvalidObjectException;
-    List<String> getErrors();
+public interface Validator<T> {
+
+    /**
+     * Valida um objeto.
+     *
+     * @throws InvalidObjectException Indica que o objeto informado não é válido.
+     */
+    void validate(T object) throws InvalidObjectException;
+
+    /**
+     * @return Erros encontrados na validação do objeto.
+     */
+    Map<String, String> getErrors();
+
 }
