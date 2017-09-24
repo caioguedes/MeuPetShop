bf5c956d2fcc64d5984897e9d5e8b51377250933
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-09-24 17:37:59 -0300

    Criando servlet para remoção de Pet.

---
+++ src/main/java/br/com/ricardosander/meupetshop/servlets/pets/PetRemove.java
@@ -0,0 +1,87 @@
+package br.com.ricardosander.meupetshop.servlets.pets;
+
+import br.com.ricardosander.meupetshop.dao.PetDAO;
+import br.com.ricardosander.meupetshop.dao.PetDAOProvider;
+import br.com.ricardosander.meupetshop.model.Pet;
+import br.com.ricardosander.meupetshop.model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/pet/remove")
+public class PetRemove extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String previouslyPage = req.getHeader("Referer");
+
+        long id = 0;
+
+        User user = (User) req.getSession().getAttribute("loggedUser");
+
+        try {
+            id = Long.parseLong(req.getParameter("id"));
+        } catch (Exception exception) {
+            user.addFlashMessage("message", "Pet não informado.");
+            resp.sendRedirect("/pets");
+            return;
+        }
+
+        PetDAO petDAO = new PetDAOProvider().newPetDAO();
+        Pet pet = petDAO.find(user, id);
+
+        if (pet == null) {
+            user.addFlashMessage("message", "Pet não encontrado.");
+            resp.sendRedirect("/pets");
+            return;
+        }
+
+        req.setAttribute("pet", pet);
+        req.setAttribute("previouslyPage", previouslyPage);
+
+        req.getRequestDispatcher("/WEB-INF/pages/pets/remove.jsp").forward(req, resp);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String previouslyPage = req.getHeader("Referer");
+
+        long id = 0;
+
+        User user = (User) req.getSession().getAttribute("loggedUser");
+
+        try {
+            id = Long.parseLong(req.getParameter("id"));
+        } catch (Exception exception) {
+            user.addFlashMessage("message", "Pet não informado.");
+            resp.sendRedirect("/pets");
+            return;
+        }
+
+        PetDAO petDAO = new PetDAOProvider().newPetDAO();
+        Pet pet = petDAO.find(user, id);
+
+        if (pet == null) {
+            user.addFlashMessage("message", "Pet não encontrado.");
+            resp.sendRedirect("/pets");
+            return;
+        }
+
+        if (!petDAO.remove(pet)) {
+            user.addFlashMessage("message", "Não foi possível remover o pet.");
+            resp.sendRedirect(previouslyPage);
+            return;
+        }
+
+        user.addFlashMessage("message", "Pet removido com sucesso.");
+        resp.sendRedirect("/pets");
+    }
+
+}
