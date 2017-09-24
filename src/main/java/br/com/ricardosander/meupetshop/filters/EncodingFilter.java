d655fa5ff4e1b02a57357d9ffec01777f2e8f4ee
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-09-24 15:13:37 -0300

    Criando novo filtro para setar encoding da requisição. Removendo esta responsabilidade do AuthFilter.

---
+++ src/main/java/br/com/ricardosander/meupetshop/filters/EncodingFilter.java
@@ -0,0 +1,27 @@
+package br.com.ricardosander.meupetshop.filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/*")
+public class EncodingFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        servletRequest.setCharacterEncoding("UTF-8");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+}
