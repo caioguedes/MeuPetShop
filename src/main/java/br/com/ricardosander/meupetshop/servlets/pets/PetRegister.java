a251a48aefb6f9c6f4a4bbc845bcb0b20f9774cf
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-10-03 22:29:23 -0300

    Simplificando classe pelo uso do filtro de Pet.

--- src/main/java/br/com/ricardosander/meupetshop/servlets/pets/PetRegister.java
+++ src/main/java/br/com/ricardosander/meupetshop/servlets/pets/PetRegister.java
@@ -3,8 +3,6 @@ package br.com.ricardosander.meupetshop.servlets.pets;
 import br.com.ricardosander.meupetshop.dao.PetDAO;
 import br.com.ricardosander.meupetshop.dao.PetDAOProvider;
 import br.com.ricardosander.meupetshop.model.*;
-import br.com.ricardosander.meupetshop.validator.PetValidator;
-import br.com.ricardosander.meupetshop.validator.Validator;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,18 +10,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.Map;
 
 @WebServlet(urlPatterns = "/pets/register")
 public class PetRegister extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        User user = (User) req.getSession().getAttribute("loggedUser");
 
         req.setAttribute("dateFormatter", DateTimeFormatter.ofPattern("dd/MM/yyyy"));
         req.setAttribute("M", Gender.M);
@@ -36,114 +29,7 @@ public class PetRegister extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        Map<String, Object> errors = new HashMap<>();
-
-        String name = req.getParameter("name");
-        String species = req.getParameter("species");
-        String breed = req.getParameter("breed");
-        String fur = req.getParameter("fur");
-        String pelage = req.getParameter("pelage");
-
-        PetSize size;
-        try {
-            size = PetSize.valueOf(req.getParameter("size"));
-        } catch (Exception exception) {
-            size = null;
-        }
-
-        String weightString = req.getParameter("weight");
-        String comments = req.getParameter("comments");
-
-        Gender gender;
-        try {
-            gender = Gender.valueOf(req.getParameter("gender"));
-        } catch (Exception exception) {
-            gender = null;
-        }
-
-        String registerString = req.getParameter("register");
-        String birthString = req.getParameter("birth");
-        String castratedString = req.getParameter("castrated");
-        String clientPacketString = req.getParameter("clientPacket");
-        String ownerIdString = req.getParameter("owner_id");
-
-        double weight = 0.0;
-        try {
-            weight = Double.parseDouble(weightString.replace(",", "."));
-        } catch (Exception exception) {
-            errors.put("pet_weight", "O campo Peso deve ser um valor numérico válido.");
-        }
-
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-        LocalDate register = null;
-        try {
-
-            if (registerString != null && !registerString.trim().isEmpty()) {
-                register = LocalDate.parse(registerString, dateTimeFormatter);
-            }
-
-        } catch (Exception exception) {
-            errors.put("pet_register", "O campo Cadastro é de preenchimento obrigatório.");
-        }
-
-        LocalDate birth = null;
-        try {
-
-            if (birthString != null && !birthString.trim().isEmpty()) {
-                birth = LocalDate.parse(birthString, dateTimeFormatter);
-            }
-
-        } catch (Exception exception) {
-            errors.put("pet_birth", "O campo Nascimento informado é inválido.");
-        }
-
-        boolean castrated = castratedString != null;
-
-        boolean clientPacket = clientPacketString != null;
-
-        long ownerId = 0;
-        Owner owner;
-        if (ownerIdString != null && !ownerIdString.trim().isEmpty()) {
-
-            try {
-                ownerId = Integer.parseInt(ownerIdString.trim());
-            } catch (Exception exception) {
-                //TODO tratar.
-            }
-            //TODO buscar owner.
-        }
-
-        User user = (User) req.getSession().getAttribute("loggedUser");
-
-        Pet pet = new Pet(
-                name,
-                species,
-                breed,
-                fur,
-                pelage,
-                size,
-                weight,
-                birth,
-                register,
-                castrated,
-                comments,
-                gender,
-                clientPacket,
-                user
-        );
-
-        Validator validator = new PetValidator();
-        errors.putAll(validator.validate(pet));
-
-        if (!errors.isEmpty()) {
-
-            errors.put("pet", pet);
-            errors.forEach(req.getSession()::setAttribute);
-
-            resp.sendRedirect("/pets/register");
-            return;
-        }
+        Pet pet = (Pet) req.getAttribute("pet");
 
         PetDAO petDAO = new PetDAOProvider().newPetDAO();
         if (!petDAO.insert(pet)) {
