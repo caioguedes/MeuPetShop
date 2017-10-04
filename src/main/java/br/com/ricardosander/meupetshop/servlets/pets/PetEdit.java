fff4bdad58979848b883e80fd73f3ee67be8ead9
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-10-03 22:46:48 -0300

    Ajustando conforme uso do filtro.

--- src/main/java/br/com/ricardosander/meupetshop/servlets/pets/PetEdit.java
+++ src/main/java/br/com/ricardosander/meupetshop/servlets/pets/PetEdit.java
@@ -17,13 +17,16 @@ import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
-@WebServlet (urlPatterns = "/pets/edit")
+@WebServlet(urlPatterns = "/pets/edit")
 public class PetEdit extends HttpServlet {
 
     private boolean isParametersValid(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
         if (req.getParameter("id") == null || req.getParameter("id").isEmpty()) {
-            req.getSession().setAttribute("message", "Ops, não foi possível encontrar o pet selecionado!");
+
+            FlashMessage flashMessage = (FlashMessage) req.getSession().getAttribute("flash_message");
+            flashMessage.add("message", "Ops, não foi possível encontrar o pet selecionado!");
+
             resp.sendRedirect("/pets");
             return false;
         }
@@ -35,21 +38,22 @@ public class PetEdit extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         User user = (User) req.getSession().getAttribute("loggedUser");
-        Pet pet = null;
 
         if (!this.isParametersValid(req, resp)) {
             return;
         }
 
         int petId = Integer.parseInt(req.getParameter("id"));
-        if (req.getSession().getAttribute("pet") != null) {
-            pet = (Pet) req.getSession().getAttribute("pet");
-        } else {
+        Pet pet = (Pet) req.getAttribute("pet");
+        if (pet == null || pet.getId() != petId) {
             pet = new PetDAOProvider().newPetDAO().find(user, petId);
         }
 
         if (pet == null) {
-            req.getSession().setAttribute("message", "Ops, não foi possível encontrar o pet selecionado!");
+
+            FlashMessage flashMessage = (FlashMessage) req.getSession().getAttribute("flash_message");
+            flashMessage.add("message", "Ops, não foi possível encontrar o pet selecionado!");
+
             resp.sendRedirect("/pets");
             return;
         }
@@ -66,129 +70,21 @@ public class PetEdit extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        Map<String, Object> errors = new HashMap<>();
-
-        if (!this.isParametersValid(req, resp)) {
-            return;
-        }
-
-        int id = Integer.parseInt(req.getParameter("id"));
-        String name = req.getParameter("name");
-        String species = req.getParameter("species");
-        String breed = req.getParameter("breed");
-        String fur = req.getParameter("fur");
-        String pelage = req.getParameter("pelage");
-
-        PetSize size;
-        try {
-            size = PetSize.valueOf(req.getParameter("size"));
-        } catch (Exception exception) {
-            size = null;
-        }
-
-        String weightString = req.getParameter("weight");
-        String comments = req.getParameter("comments");
-
-        Gender gender;
-        try {
-            gender = Gender.valueOf(req.getParameter("gender"));
-        } catch (Exception exception) {
-            gender = null;
-        }
-
-        String registerString = req.getParameter("register");
-        String birthString = req.getParameter("birth");
-        String castratedString = req.getParameter("castrated");
-        String clientPacketString = req.getParameter("clientPacket");
-        String ownerIdString = req.getParameter("owner_id");
-
-        double weight = 0.0;
-        try {
-            weight = Double.parseDouble(weightString.replace(",", "."));
-        } catch (Exception exception) {
-            errors.put("pet_weight", "O campo Peso deve ser um valor numérico válido.");
-        }
-
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-        LocalDate register = null;
-        try {
-
-            if (registerString != null && !registerString.trim().isEmpty()) {
-                register = LocalDate.parse(registerString, dateTimeFormatter);
-            }
-
-        } catch (Exception exception) {
-            errors.put("pet_register", "O campo Cadastro é de preenchimento obrigatório.");
-        }
-
-        LocalDate birth = null;
-        try {
-
-            if (birthString != null && !birthString.trim().isEmpty()) {
-                birth = LocalDate.parse(birthString, dateTimeFormatter);
-            }
-
-        } catch (Exception exception) {
-            errors.put("pet_birth", "O campo Nascimento informado é inválido.");
-        }
-
-        boolean castrated = castratedString != null;
-
-        boolean clientPacket = clientPacketString != null;
-
-        long ownerId = 0;
-        Owner owner;
-        if (ownerIdString != null && !ownerIdString.trim().isEmpty()) {
-
-            try {
-                ownerId = Integer.parseInt(ownerIdString.trim());
-            } catch (Exception exception) {
-                //TODO tratar.
-            }
-            //TODO buscar owner.
-        }
-
-        User user = (User) req.getSession().getAttribute("loggedUser");
-
-        Pet pet = new Pet(
-                name,
-                species,
-                breed,
-                fur,
-                pelage,
-                size,
-                weight,
-                birth,
-                register,
-                castrated,
-                comments,
-                gender,
-                clientPacket,
-                user
-        );
-        pet.setId(id);
-
-        Validator validator = new PetValidator();
-        errors.putAll(validator.validate(pet));
-
-        if (!errors.isEmpty()) {
-
-            errors.put("pet", pet);
-            errors.forEach(req.getSession()::setAttribute);
-
-            resp.sendRedirect("/pets/edit?id=" + id);
-            return;
-        }
+        Pet pet = (Pet) req.getAttribute("pet");
 
         PetDAO petDAO = new PetDAOProvider().newPetDAO();
         if (!petDAO.update(pet)) {
-            req.getSession().setAttribute("message", "Houve um erro ao salvar o Pet.");
-            resp.sendRedirect("/pets/edit?id=" + id);
+
+            FlashMessage flashMessage = (FlashMessage) req.getSession().getAttribute("flash_message");
+            flashMessage.add("message", "Houve um erro ao salvar o Pet.");
+
+            resp.sendRedirect(req.getHeader("referer"));
             return;
         }
 
-        req.getSession().setAttribute("message", "As informações do pet foram atualizadas!");
+        FlashMessage flashMessage = (FlashMessage) req.getSession().getAttribute("flash_message");
+        flashMessage.add("message", "As informações do pet foram atualizadas!");
+
         resp.sendRedirect("/pet?id=" + pet.getId());
     }
 }
