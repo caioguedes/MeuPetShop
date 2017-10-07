711bed6a6b0dd21bc6c30583337faf23a8352b05
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-10-07 03:15:06 -0300

    Builder para criteria de pet.

---
+++ src/main/java/br/com/ricardosander/meupetshop/criteria/PetCriteriaBuilder.java
@@ -0,0 +1,33 @@
+package br.com.ricardosander.meupetshop.criteria;
+
+/**
+ * Builder para criteria de pet.
+ */
+public class PetCriteriaBuilder {
+
+    private int limit;
+
+    private int offset;
+
+    private String name;
+
+    public PetCriteriaBuilder limit(int limit) {
+        this.limit = limit;
+        return this;
+    }
+
+    public PetCriteriaBuilder offset(int offset) {
+        this.offset = offset;
+        return this;
+    }
+
+    public PetCriteriaBuilder name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public PetCriteria build() {
+        return new PetCriteria(limit, offset, name);
+    }
+
+}
