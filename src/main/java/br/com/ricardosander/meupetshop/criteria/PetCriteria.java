4e9c1bbf86ebd6ce85b5b4c0cbc8373f403cd6d0
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-10-07 03:14:22 -0300

    Criteria para busca de pet.

---
+++ src/main/java/br/com/ricardosander/meupetshop/criteria/PetCriteria.java
@@ -0,0 +1,54 @@
+package br.com.ricardosander.meupetshop.criteria;
+
+/**
+ * Padrão critéria para buscas de Pet.
+ */
+public class PetCriteria {
+
+    /**
+     * Limit da busca.
+     */
+    private final int limit;
+
+    /**
+     * Inicio da busca.
+     */
+    private final int offset;
+
+    /**
+     * Nome da busca do pet.
+     */
+    private final String name;
+
+    /**
+     * @param limit  Limit da busca.
+     * @param offset Inicio da busca.
+     * @param name   Nome do pet para busca.
+     */
+    PetCriteria(int limit, int offset, String name) {
+        this.limit = limit;
+        this.offset = offset;
+        this.name = name;
+    }
+
+    /**
+     * @return Limit da busca.
+     */
+    public int getLimit() {
+        return limit;
+    }
+
+    /**
+     * @return Inicio da busca.
+     */
+    public int getOffset() {
+        return offset;
+    }
+
+    /**
+     * @return Nome da busca do pet.
+     */
+    public String getName() {
+        return name;
+    }
+}
