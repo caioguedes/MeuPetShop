4aec8e62b5250562d9d4e234a130be9667f8742e
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-10-08 18:25:04 -0300

    Criando classe que irá auxiliar na paginação.

---
+++ src/main/java/br/com/ricardosander/meupetshop/util/Paginator.java
@@ -0,0 +1,149 @@
+package br.com.ricardosander.meupetshop.util;
+
+/**
+ * Classe que auxiliar na paginação.
+ */
+public class Paginator {
+
+    /**
+     * Valor padrão de registros por páginas.
+     */
+    private static final int DEFAULT_REGISTERS_PER_PAGE = 20;
+
+    /**
+     * Página atual.
+     */
+    private final int currentPage;
+
+    /**
+     * Total de registros.
+     */
+    private final int totalRegister;
+
+    /**
+     * Número de registros por página.
+     */
+    private final int registersPerPage;
+
+    /**
+     * @param totalRegister Total de registros.
+     */
+    public Paginator(int totalRegister) {
+        this.currentPage = this.filterCurrentPage(1);
+        this.totalRegister = totalRegister;
+        this.registersPerPage = DEFAULT_REGISTERS_PER_PAGE;
+    }
+
+    /**
+     * @param currentPage   Página atual.
+     * @param totalRegister Total de registros.
+     */
+    public Paginator(int currentPage, int totalRegister) {
+        this.currentPage = this.filterCurrentPage(currentPage);
+        this.totalRegister = this.filterTotalRegister(totalRegister);
+        this.registersPerPage = DEFAULT_REGISTERS_PER_PAGE;
+    }
+
+    /**
+     * @param currentPage      Página atual.
+     * @param totalRegister    Total de registros.
+     * @param registersPerPage Registros por página.
+     */
+    public Paginator(int currentPage, int totalRegister, int registersPerPage) {
+        this.currentPage = this.filterCurrentPage(currentPage);
+        this.totalRegister = this.filterTotalRegister(totalRegister);
+        this.registersPerPage = this.filterRegisterPerPage(registersPerPage);
+    }
+
+    /**
+     * @return Página atual.
+     */
+    public int getCurrentPage() {
+        return currentPage;
+    }
+
+    /**
+     * @return Total de registros.
+     */
+    public int getTotalRegister() {
+        return totalRegister;
+    }
+
+    /**
+     * @return Primeira página na listagem.
+     */
+    public int getFirstListedPage() {
+
+        if (currentPage < 3) {
+            return 1;
+        }
+        return currentPage - 2;
+    }
+
+    /**
+     * @return ùltima página na listagem.
+     */
+    public int getLastListedPage() {
+
+        if (currentPage > (this.getLastPage() - 2)) {
+            return this.getLastPage();
+        }
+        return currentPage + 2;
+    }
+
+    /**
+     * @return Primeira página.
+     */
+    public int getFirstPage() {
+        return 1;
+    }
+
+    /**
+     * @return Última página.
+     */
+    public int getLastPage() {
+        return this.getPages();
+    }
+
+    /**
+     * @return Número de páginas.
+     */
+    public int getPages() {
+
+        if (totalRegister % registersPerPage != 0) {
+            return totalRegister / registersPerPage + 1;
+        }
+        return totalRegister / registersPerPage;
+    }
+
+    /**
+     * Válida o valor informado para o atributo currentPage e retorna um valor válido.
+     *
+     * @param currentPage Valor candidato para o atributo.
+     * @return Valor válido para o atributo.
+     */
+    private int filterCurrentPage(int currentPage) {
+        return currentPage > 0 ? currentPage : 1;
+    }
+
+    /**
+     * Válida o valor informado para o atributo registerPerPage e retorna um valor válido.
+     *
+     * @param registersPerPage Valor candidato para o atributo.
+     * @return Valor válido para o atributo.
+     */
+    private int filterRegisterPerPage(int registersPerPage) {
+        return registersPerPage > 0 ? registersPerPage : DEFAULT_REGISTERS_PER_PAGE;
+    }
+
+    /**
+     * Válida o valor informado para o atributo totalRegister e retorna um valor válido.
+     *
+     * @param totalRegister Valor candidato para o atributo.
+     * @return Valor válido para o atributo.
+     */
+    private int filterTotalRegister(int totalRegister) {
+        return totalRegister < 0 ? 0 : totalRegister;
+    }
+
+}
