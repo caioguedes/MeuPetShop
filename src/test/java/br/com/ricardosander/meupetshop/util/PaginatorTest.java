cfb58cd7b18dad5c1c5f0a73b990c51feadfb868
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2017-10-14 20:36:55 -0300

    Adicionando teste para mais um bug encontrada na paginação.

--- src/test/java/br/com/ricardosander/meupetshop/util/PaginatorTest.java
+++ src/test/java/br/com/ricardosander/meupetshop/util/PaginatorTest.java
@@ -30,4 +30,28 @@ public class PaginatorTest {
 
     }
 
+    @Test
+    public void test77RegisterThirdPage() {
+
+        Paginator paginator = new Paginator(3, 77);
+
+        assertEquals(77, paginator.getTotalRegister());
+        assertEquals(20, paginator.getRegistersPerPage());
+
+        assertEquals(3, paginator.getCurrentPage());
+        assertEquals(2, paginator.getPreviousPage());
+        assertEquals(4, paginator.getNextPage());
+
+        assertEquals(4, paginator.getPages());
+
+        assertEquals(1, paginator.getFirstPage());
+        assertEquals(4, paginator.getLastPage());
+
+        assertEquals(1, paginator.getFirstListedPage());
+        assertEquals(4, paginator.getLastListedPage());
+
+        assertEquals(40, paginator.getOffSet());
+
+    }
+
 }
