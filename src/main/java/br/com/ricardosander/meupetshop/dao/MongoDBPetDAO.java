be77ab7428ed49be43b56f872b0470038c337644
ricardosander <ricardo.sander.lopes@gmail.com>
2018-02-19 23:12:06 -0300

    Use converter implementation to create Pet from Document and Document from Pet.

--- src/main/java/br/com/ricardosander/meupetshop/dao/MongoDBPetDAO.java
+++ src/main/java/br/com/ricardosander/meupetshop/dao/MongoDBPetDAO.java
@@ -1,10 +1,11 @@
 package br.com.ricardosander.meupetshop.dao;
 
+import br.com.ricardosander.meupetshop.conveter.DocumentToPetConverter;
+import br.com.ricardosander.meupetshop.conveter.InsertPetToDocumentConverter;
+import br.com.ricardosander.meupetshop.conveter.UpdatePetToDocumentConverter;
 import br.com.ricardosander.meupetshop.criteria.Criteria;
 import br.com.ricardosander.meupetshop.database.DatabaseMongoDB;
-import br.com.ricardosander.meupetshop.model.Gender;
 import br.com.ricardosander.meupetshop.model.Pet;
-import br.com.ricardosander.meupetshop.model.PetSize;
 import br.com.ricardosander.meupetshop.model.User;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
@@ -13,8 +14,6 @@ import com.mongodb.client.result.DeleteResult;
 import com.mongodb.client.result.UpdateResult;
 import org.bson.Document;
 
-import java.time.ZoneId;
-import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -52,6 +51,7 @@ public class MongoDBPetDAO implements PetDAO {
     public List<Pet> find(User user, Criteria criteria) {
 
         List<Pet> pets = new LinkedList<>();
+        DocumentToPetConverter petConverter = new DocumentToPetConverter(user, null);
 
         Document query = createQuery(user, criteria);
 
@@ -61,24 +61,7 @@ public class MongoDBPetDAO implements PetDAO {
                 .limit(criteria.getLimit());
 
         for (Document document : documents) {
-            pets.add(new Pet(
-                    document.getLong("_id"),
-                    document.getString("name"),
-                    document.getString("species"),
-                    document.getString("breed"),
-                    document.getString("fur"),
-                    document.getString("pelage"),
-                    PetSize.valueOf(document.getString("size")),
-                    document.getDouble("weight"),
-                    document.getDate("birth").toInstant().atZone(ZoneId.systemDefault()).toLocalDate(),
-                    document.getDate("register").toInstant().atZone(ZoneId.systemDefault()).toLocalDate(),
-                    document.getBoolean("castrated"),
-                    document.getString("comments"),
-                    Gender.valueOf(document.getString("gender").charAt(0) + ""),
-                    document.getBoolean("clientPacket"),
-                    user,
-                    null
-            ));
+            pets.add(petConverter.convert(document));
         }
 
         return pets;
@@ -89,55 +72,26 @@ public class MongoDBPetDAO implements PetDAO {
 
         Document query = new Document("_id", id).append("user", user.getId());
 
+        DocumentToPetConverter petConverter = new DocumentToPetConverter(user, null);
+
         Document first = collection.find(query).first();
 
         if (first == null) {
             return null;
         }
 
-        return new Pet(
-                first.getLong("_id"),
-                first.getString("name"),
-                first.getString("species"),
-                first.getString("breed"),
-                first.getString("fur"),
-                first.getString("pelage"),
-                PetSize.valueOf(first.getString("size")),
-                first.getDouble("weight"),
-                first.getDate("birth").toInstant().atZone(ZoneId.systemDefault()).toLocalDate(),
-                first.getDate("register").toInstant().atZone(ZoneId.systemDefault()).toLocalDate(),
-                first.getBoolean("castrated"),
-                first.getString("comments"),
-                Gender.valueOf(first.getString("gender")),
-                first.getBoolean("clientPacket"),
-                user,
-                null);
+        return petConverter.convert(first);
     }
 
     @Override
     public boolean insert(Pet pet) {
 
+        InsertPetToDocumentConverter converter = new InsertPetToDocumentConverter();
+
         MongoDatabase database = DatabaseMongoDB.getInstance();
-        final long sequence = new MongoDBSequenceDAO(database).nextValue(collectionName);
+        pet.setId(new MongoDBSequenceDAO(database).nextValue(collectionName));
 
-        Document document = new Document("name", pet.getName())
-                .append("_id", sequence)
-                .append("species", pet.getSpecies())
-                .append("breed", pet.getBreed())
-                .append("fur", pet.getFur())
-                .append("pelage", pet.getPelage())
-                .append("size", pet.getSize().name())
-                .append("weight", pet.getWeight())
-                .append("birth", Date.from(pet.getBirth().atStartOfDay(ZoneId.systemDefault()).toInstant()))
-                .append("register", Date.from(pet.getRegister().atStartOfDay(ZoneId.systemDefault()).toInstant()))
-                .append("castrated", pet.isCastrated())
-                .append("comments", pet.getComments())
-                .append("gender", pet.getGender().name())
-                .append("clientPacket", pet.isClientPacket())
-                .append("user", pet.getUser().getId())
-                .append("owner", null);
-
-        collection.insertOne(document);
+        collection.insertOne(converter.convert(pet));
 
         return true;
     }
@@ -155,24 +109,11 @@ public class MongoDBPetDAO implements PetDAO {
     @Override
     public boolean update(Pet pet) {
 
+        UpdatePetToDocumentConverter converter = new UpdatePetToDocumentConverter();
+
         Document query = new Document("_id", pet.getId()).append("user", pet.getUser().getId());
 
-        Document document = new Document("name", pet.getName())
-                .append("species", pet.getSpecies())
-                .append("breed", pet.getBreed())
-                .append("fur", pet.getFur())
-                .append("pelage", pet.getPelage())
-                .append("size", pet.getSize().name())
-                .append("weight", pet.getWeight())
-                .append("birth", Date.from(pet.getBirth().atStartOfDay(ZoneId.systemDefault()).toInstant()))
-                .append("register", Date.from(pet.getRegister().atStartOfDay(ZoneId.systemDefault()).toInstant()))
-                .append("castrated", pet.isCastrated())
-                .append("comments", pet.getComments())
-                .append("gender", pet.getGender().name())
-                .append("clientPacket", pet.isClientPacket())
-                .append("owner", null);
-
-        UpdateResult updateResult = collection.updateOne(query, new Document("$set", document));
+        UpdateResult updateResult = collection.updateOne(query, new Document("$set", converter.convert(pet)));
 
         return updateResult.getModifiedCount() == 1;
     }
